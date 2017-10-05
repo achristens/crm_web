eae0c1b4916c9320725c67c693164b6a09fdd0e6
Abby Christens <achristens@gmail.com>
2017-10-05 15:14:15 -0400

    Adding edit function for each contact, redirect to contact page after submission

--- crm.rb
+++ crm.rb
@@ -38,6 +38,31 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # Keep this line at the bottom
 after do
   ActiveRecord::Base.connection.close
