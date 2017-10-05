6cd9c9573ecd3b1d3104abe6ab87d379b1b8bd99
Abby Christens <achristens@gmail.com>
2017-10-05 15:35:22 -0400

    Deleting contact

--- crm.rb
+++ crm.rb
@@ -63,6 +63,16 @@ put '/contacts/:id' do
   end
 end
 
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # Keep this line at the bottom
 after do
   ActiveRecord::Base.connection.close
